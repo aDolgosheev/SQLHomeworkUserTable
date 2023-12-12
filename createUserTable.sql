ccba81a150d6176e2b8c33840f82e9ec72fa20f4
Aleksandr Dolgosheev <al.dolgosheev@gmail.com>
2023-12-12 13:58:49 +0300

    update 2023.12.12

--- createUserTable.sql
+++ createUserTable.sql
@@ -5,7 +5,7 @@ create table dolgosheev.persons
     name           varchar(255),
     surname        varchar(255),
     age            int,
-    phone_number   int,
+    phone_number   varchar(255),
     city_of_living varchar(255),
     primary key (name, surname, age)
 )
